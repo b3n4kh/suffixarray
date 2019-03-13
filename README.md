f8611c9c58cab0859bfb3024079c02a209068fe8
Benjamin Akhras <b@akhras.at>
2019-03-13 13:40:58 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Super Verbose Suffixarray
+
+This is a fork of [index/suffixarray](https://github.com/golang/go/tree/master/src/index/suffixarray) but aims to be very verbose in what it does.
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"index/suffixarray"
+	"strings"
+)
+
+func mysuffix() {
+	data := []byte("CAGGGTAGATGTATACGT")
+	lookup := []byte("ATG")
+	length := len(lookup)
+	index := suffixarray.New(data)
+	offsets := index.Lookup(lookup, -1)
+	fmt.Println(string(data))
+	for _, offset := range offsets {
+		spaces := strings.Repeat(" ", offset)
+		fmt.Printf("%s%s", spaces, string(data[offset:offset+length]))
+	}
+}
+
+func suffix() {
+	data := []byte("CAGGGTAGATGTATACGT")
+	lookup := []byte("ATG")
+	length := len(lookup)
+	index := suffixarray.New(data)
+	offsets := index.Lookup(lookup, -1)
+	fmt.Println(string(data))
+	for _, offset := range offsets {
+		spaces := strings.Repeat(" ", offset)
+		fmt.Printf("%s%s", spaces, string(data[offset:offset+length]))
+	}
+}
+
+func main() {
+	suffix()
+	mysuffix()
+}
